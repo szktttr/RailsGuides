c668b9b903e0257f7fa16f40289f2fd1741f2fda
テリー鈴木 <terry@mbp.local>
2018-07-18 20:07:05 +0900

    railsguides chapter9 finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "testman", password: "testman1", except: [:index, :show]
+
   def new
     @article = Article.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root  'welcome#index'
   get   'welcome/index'
  # get   'article/show'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
